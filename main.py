6db88381b2544114c58772c0bc40d695c4be74b5
Narges <narges.moharrami97@gmail.com>
2022-01-06 12:28:36 -0800

    error in find_winner

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from random import choice
 
-GAME_CHOICES = ("r", "p", "s")  # rock, paper, scisseor
-RULLES = {
-    ("p", "r"): "p",
-    ("p", "s"): "s",
-    ("r", "s"): "r"
-}
+GAME_CHOICES = ("r", "p", "s")  # rock, paper, scissor
 
 
 class Player:
@@ -15,25 +10,36 @@ class Player:
     @staticmethod
     def get_user_choice():
         user_input = input("enter your choice please: ")
-        if user_input is not GAME_CHOICES:
+        if user_input not in GAME_CHOICES:
             print("oops! wrong choice, try again please")
-            Player.get_user_choice()
+            user_input = Player.get_user_choice()
         return user_input
 
 
 class System:
-    @staticmethod
-    def get_system_choice():
-        return choice(GAME_CHOICES)
+    @classmethod
+    def get_system_choice(cls):
+        return choice(("r", "p", "s"))
 
 
 class Winner:
-    @staticmethod
-    def find_winner():
-        match = {Player.get_user_choice(), System.get_system_choice()}
+    RULES = {
+        ('r', 'p'): 'p',
+        ('p', 's'): 's',
+        ('r', 's'): 'r',
+    }
+
+    def __init__(self):
+        self.player = Player.get_user_choice()
+        self.system = System.get_system_choice()
+
+    def find_winner(self):
+        match = {self.player, self.system}
         if len(match) == 1:
+            print("equal")
             return None
-        return RULLES[tuple(sorted(match))]
+        print(tuple(sorted(match)))
+        return self.RULES[tuple(sorted(match))]
 
 
 class PlayOnHand:
@@ -50,4 +56,6 @@ class Play:
 
 
 if __name__ == "__main__":
+    winner = Winner()
     play = Play()
+    winner.find_winner()
